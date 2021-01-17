d3d55dfa38392f717bfd7764e8cc6cc2f4b7278a
Luigi Schiavone <luigisc98@gmail.com>
2021-01-17 18:38:04 +0100

    Icons added.

--- src/app/services/chat-notifications.service.ts
+++ src/app/services/chat-notifications.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { SwPush } from '@angular/service-worker';
-import { ChatCoreService } from './chat-core.service';
+
+const pnsUri = 'https://p74319.deta.dev';
 
 @Injectable({
   providedIn: 'root'
@@ -9,20 +10,21 @@ import { ChatCoreService } from './chat-core.service';
 export class ChatNotificationsService {
 
   readonly VAPID_PUBLIC_KEY = "BP07KrtTn9S37-WusRWp-Y2OBWcFmo8rG0wb5hjhcYbArBd5xdNa2qkFLesxwKhidy0_t213r5ZEk0eeLO05Ol4";
+  
   username: string;
 
-  constructor(private swPush: SwPush, private http: HttpClient) { 
+  constructor(public swPush: SwPush, private http: HttpClient) { 
 
   }
 
   unsubscribeToMessagesPushNotifications(){
     this.swPush.subscription.subscribe(currentSub => {
       if (currentSub){
-        console.log("CNS: unsubscribed from push notifications service");
+        //console.log("CNS: unsubscribed from push notifications service");
         let request = {'subscription_info': currentSub, 'username': this.username};
         let subJSON = JSON.stringify(request);
-        console.log("CNS: requesting unsubscription with subBody", subJSON);
-        this.http.post("http://127.0.0.1:8000/messages/push/unsubscribe", JSON.parse(subJSON)).subscribe(data => {
+        //console.log("CNS: requesting unsubscription with subBody", subJSON);
+        this.http.post(pnsUri + "/messages/push/unsubscribe", JSON.parse(subJSON)).subscribe(data => {
           this.swPush.unsubscribe();
           console.log("CNS: unsubscribed from push notifications server with response", data);
         });
@@ -38,17 +40,17 @@ export class ChatNotificationsService {
     .then(sub => {
       let request = {'subscription_info': sub, 'username': username}
       let subJSON = JSON.stringify(request);
-      this.http.post("http://127.0.0.1:8000/messages/push/subscribe", JSON.parse(subJSON)).subscribe(data => {
-        console.log("CNS: subscription response", data);
+      this.http.post(pnsUri + "/messages/push/subscribe", JSON.parse(subJSON)).subscribe(data => {
+        console.log("CNS: subscription request response from the server", data);
       });
     })//this.newsletterService.addPushSubscriber(sub).subscribe())
-    .catch(err => console.error("CNS: could not subscribe to notifications", err));
+    .catch(err => console.error("CNS: could not subscribe to message notifications server", err));
     }
 
   sendMessagePushNotification(text, currentUsername, targetUsername){
     let json = JSON.stringify({"text": text, "senderUsername": currentUsername, "receiverUsername": targetUsername});
-      this.http.post("http://127.0.0.1:8000/messages/push", JSON.parse(json)).subscribe(data => {
-        console.log("Response", data);
+      this.http.post(pnsUri + "/messages/push", JSON.parse(json)).subscribe(data => {
+        console.log("CNS: message push notification sent to server with response", data);
       });
   }
 }
