1e977fd2349a5c357c2bb8a801095ebcfdc88b7a
Luigi Schiavone <luigisc98@gmail.com>
2021-04-13 22:22:06 +0200

    Fixed config problems.

---
+++ firebaseData.ts
@@ -0,0 +1,11 @@
+
+export const angularFireModuleData = {
+  apiKey: "AIzaSyAA87TyI0UwyTGlcfB4QHnp9OaLcHVL9js",
+  authDomain: "hi-chat-bec8f.firebaseapp.com",
+  databaseURL: "https://hi-chat-bec8f-default-rtdb.firebaseio.com",
+  projectId: "hi-chat-bec8f",
+  storageBucket: "hi-chat-bec8f.appspot.com",
+  messagingSenderId: "813116576758",
+  appId: "1:813116576758:web:2a8f3eb8b874e9dc7badd7",
+  measurementId: "G-F5YKMYKXCG"
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -19,6 +19,7 @@ import { BackButtonDisableModule } from 'angular-disable-browser-back-button';
 import {NgxHowlerService} from 'ngx-howler';
 import { AngularFireStorageModule } from '@angular/fire/storage';
 import { AngularFireModule } from '@angular/fire';
+import { angularFireModuleData } from '../../firebaseData';
 
 @NgModule({
   declarations: [
@@ -58,16 +59,7 @@ import { AngularFireModule } from '@angular/fire';
     BackButtonDisableModule.forRoot(),
     NbContextMenuModule,
     NbMenuModule.forRoot(),
-    AngularFireModule.initializeApp({
-      apiKey: "AIzaSyAA87TyI0UwyTGlcfB4QHnp9OaLcHVL9js",
-      authDomain: "hi-chat-bec8f.firebaseapp.com",
-      databaseURL: "https://hi-chat-bec8f-default-rtdb.firebaseio.com",
-      projectId: "hi-chat-bec8f",
-      storageBucket: "hi-chat-bec8f.appspot.com",
-      messagingSenderId: "813116576758",
-      appId: "1:813116576758:web:2a8f3eb8b874e9dc7badd7",
-      measurementId: "G-F5YKMYKXCG"
-    }),
+    AngularFireModule.initializeApp(angularFireModuleData),
     AngularFireStorageModule
   ],
   bootstrap: [AppComponent],
