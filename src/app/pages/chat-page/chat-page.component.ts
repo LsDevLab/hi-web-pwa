3287ff81daff2c4b6a1f2054d37b64cc51e010bf
Luigi Schiavone <luigisc98@gmail.com>
2021-06-09 12:49:49 +0200

    Fixed chat-page.component.ts

--- src/app/pages/chat-page/chat-page.component.ts
+++ src/app/pages/chat-page/chat-page.component.ts
@@ -79,10 +79,10 @@ export class ChatPageComponent implements OnInit {
     const helper = new JwtHelperService();
     const tokenExpiredInterval = setInterval(() => {
       const isTokenExpired: boolean = helper.isTokenExpired(localStorage.getItem('currentToken'));
-      if (!isTokenExpired) {
+      if (isTokenExpired) {
         this.dialogService.open(DialogTokenExpiredComponent, { closeOnBackdropClick: false, closeOnEsc: false });
         this.toastrService.show("Login into with your account again. Logging out...", "Access expired", new NbToastrConfig({status:"info"}));
-        setTimeout(() => this.auth.logout(), 4000);
+        setTimeout(() => this.auth.logout({ returnTo: document.location.origin }), 4000);
         clearInterval(tokenExpiredInterval);
       }
     }, 2000);
